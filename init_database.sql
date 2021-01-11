e5374cfea14df149cf80784855a71b7cc854a580
LuisPeMoraRod <luismorarod98@gmail.com>
2021-01-10 21:45:31 -0600

    Add init_database.sql

---
+++ init_database.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE ODYSSEY_DB;
+
+USE ODYSSEY_DB;
+DROP TABLE TRACKS;
+DROP TABLE SEARCH_HISTORY;
+
+CREATE TABLE TRACKS (
+    id VARCHAR(100) PRIMARY KEY ,
+    track_name VARCHAR(100),
+    artist VARCHAR(100),
+    album VARCHAR(100),
+    duration_ms INT,
+    release_date VARCHAR(100),
+    lyrics TEXT,
+    youtubeLink VARCHAR(100)
+);
+
+CREATE TABLE SEARCH_HISTORY(
+    searching_key VARCHAR(100) PRIMARY KEY ,
+    user_id SMALLINT
+);
